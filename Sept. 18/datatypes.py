a56593bf4d2f8709c2cfdeb0fa9101be2c0437fc
CarlTobias <113130276+CarlTobias@users.noreply.github.com>
2024-09-18 10:57:42 +0400

    first commit

---
+++ Sept. 18/datatypes.py
@@ -0,0 +1,15 @@
+# Data Types
+
+print("Variable Switch\n\n")
+
+
+userinput = int(input("Please input a number: "))
+
+if userinput % 2 == 0:
+    print("The number is even.\n")
+
+elif userinput % 2 != 0:
+    print("The number is odd.\n")
+
+else:
+    print("Error.\n")
---
+++ Sept. 18/syntaxandvariables.py
@@ -0,0 +1,19 @@
+# Syntax and Variables
+
+
+a = 16
+b = 28
+
+
+print("Variable Switch\n\n")
+
+
+print("Original:\n")
+print("A: ", int(a), "\nB: ", int(b), "\n")
+
+temp = a
+a = b
+b = temp
+
+print("Swapped:\n")
+print("A: ", int(a), "\nB: ", int(b), "\n")
